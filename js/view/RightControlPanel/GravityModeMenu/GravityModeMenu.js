b46c386f8b05d99717d8322d79d05c038215e299
Andrew Zelenkov <andrey.zelenkov@mlearner.com>
2013-09-16 13:21:51 +0400

    gravity-and-orbits: gravity menu refactoring

--- js/view/RightControlPanel/GravityModeMenu/GravityModeMenu.js
+++ js/view/RightControlPanel/GravityModeMenu/GravityModeMenu.js
@@ -9,8 +9,7 @@ define( function( require ) {
   'use strict';
   var Node = require( 'SCENERY/nodes/Node' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var RadioButton = require( 'SUN/RadioButton' );
-  var Circle = require( 'SCENERY/nodes/Circle' );
+  var AquaRadioButton = require( 'SUN/AquaRadioButton' );
 
   var Strings = require( 'Strings' );
   var Text = require( 'SCENERY/nodes/Text' );
@@ -18,30 +17,14 @@ define( function( require ) {
   var FONT = new PhetFont( 16 );
 
   function GravityModeMenu( model, coords ) {
-    var text, radio;
     Node.call( this, coords );
 
     // add "gravity" text
-    text = new Text( Strings['GAO.gravity'] + ':', { font: FONT, fill: '#fff', pickable: false } );
-    this.addChild( text );
+    this.addChild( new Text( Strings['GAO.gravity'] + ':', { font: FONT, fill: '#fff', pickable: false } ) );
 
-    // radio buttons views
-    var views = {
-      selectedNode: new Node( {children: [new Circle( 6, {fill: '#fff' } ), new Circle( 3, {fill: '#000' } )]} ),
-      deselectedNode: new Node( {children: [new Circle( 6, {fill: '#fff' } )]} )
-    };
-
-    // add on button
-    radio = new RadioButton( model.gravityProperty, true, new Node( {children: [views.selectedNode]} ), new Node( {children: [views.deselectedNode]} ), {x: 70, y: -5} );
-    this.addChild( radio );
-    text = new Text( Strings['GAO.on'], { font: FONT, fill: '#fff', pickable: false, x: 82 } );
-    this.addChild( text );
-
-    // add off button
-    radio = new RadioButton( model.gravityProperty, false, new Node( {children: [views.selectedNode]} ), new Node( {children: [views.deselectedNode]} ), {x: 120, y: -5} );
-    this.addChild( radio );
-    text = new Text( Strings['GAO.off'], { font: FONT, fill: '#fff', pickable: false, x: 132 } );
-    this.addChild( text );
+    // add on/off buttons
+    this.addChild( new AquaRadioButton( model.gravityProperty, true, new Text( Strings['GAO.on'], { font: FONT, fill: '#fff', pickable: false, x: 82 } ), {radius: 7, x: 70, y: -5} ) );
+    this.addChild( new AquaRadioButton( model.gravityProperty, false, new Text( Strings['GAO.off'], { font: FONT, fill: '#fff', pickable: false, x: 132 } ), {radius: 7, x: 120, y: -5 } ) );
   }
 
   inherit( Node, GravityModeMenu );
