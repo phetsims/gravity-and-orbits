db89e61faa7167841692e403071661d02d4a2430
Andrew Zelenkov <andrey.zelenkov@mlearner.com>
2013-12-03 13:56:50 +0400

    fixed #38, added comments, small refactoring for prevPosition keeping

--- js/view/workspace/PlanetPath.js
+++ js/view/workspace/PlanetPath.js
@@ -23,50 +23,61 @@ define( function( require ) {
     self.color = {
       sun: 'yellow',
       earth: 'gray',
-      moon: 'rgb(255,0,255)',
+      moon: 'magenta',
       spaceStation: 'gray'
     };
 
-    self.time = [];
-    self.maxTime = [400, 300, 20, 0.05];
+    self.time = []; // passed time for each mode (self.time.length === self.maxTime.length)
+    self.maxTime = [400, 300, 20, 0.05]; // max time to store path
 
-    self.clearAll( model );
+    self.clearAll( model ); // prepare component for work
     model.spaceObjects.forEach( function( el ) {
       var body = model[el];
+
+      // add position property observer
       model[el].positionProperty.link( function() {
-        if ( !body.exploded ) {
+        if ( !body.exploded && model.path && model.planetMode === self.num ) {
           var newPosition = body.position;
-          var dr = newPosition.minus( self.prevPosition[self.num][el][self.prevPosition[self.num][el].length - 1] ).magnitude();
-          if ( dr > 2 && self.visibility && model.planetMode === self.num ) {
+          var dr = newPosition.minus( self.prevPosition[self.num][el] ).magnitude();
+
+          // add new piece of path if position was changed significantly
+          if ( dr > 2 ) {
             self.add( model, el, newPosition );
           }
         }
+        // check lifetime of path
         self.check( model );
       } );
     } );
 
-    model.pathProperty.link( function( visibility ) {
+    // clear path if visibility changed
+    model.pathProperty.link( function() {
       self.clearAll( model );
-      self.visibility = visibility;
     } );
 
     model.planetModeProperty.link( function( num ) {
+      // hide previous path
       self.hide( model, self.num );
+
+      // show current path
       self.show( model, self.num = num );
     } );
 
+    // clear path if refresh was called
     model.refreshModeProperty.link( function( trigger ) {
       if ( trigger ) {
         self.clearOne( model, self.num );
       }
     } );
 
+    // clear path if rewind was called
     model.rewindProperty.link( function( isRewind ) {
       if ( isRewind ) {
         self.clearOne( model, self.num );
       }
     } );
 
+    // update time value for current mode
     model.dayProperty.link( function( newDay, oldDay ) {
       if ( model.planetMode !== self.num ) {return;}
       self.time[self.num] += (newDay - oldDay) / model.speed;
@@ -74,14 +85,16 @@ define( function( require ) {
   }
 
   return inherit( Node, PlanetPath, {
+    // add new piece of path for given element
     add: function( model, el, newPosition ) {
-      var prevPosition = this.prevPosition[this.num][el][this.prevPosition[this.num][el].length - 1],
+      var prevPosition = this.prevPosition[this.num][el],
         path = new Path( new Shape().moveTo( prevPosition.x, prevPosition.y ).lineTo( newPosition.x, newPosition.y ), {stroke: this.color[el], lineWidth: 3, lineCap: 'square'} );
 
       this.path[this.num][el].push( {view: path, time: this.time[this.num]} );
       this.addChild( path );
-      this.prevPosition[this.num][el].push( newPosition );
+      this.prevPosition[this.num][el] = newPosition.copy();
     },
+    // check lifetime of all pieces for all space objects
     check: function( model ) {
       var self = this, paths, path;
 
@@ -96,23 +109,25 @@ define( function( require ) {
         }
       } );
     },
+    // remove all path for all modes
     clearAll: function( model ) {
       var self = this;
       self.removeAllChildren();
 
-      self.num = model.planetMode;
-      self.path = [];
-      self.prevPosition = [];
+      self.num = model.planetMode; // save planet mode value
+      self.path = []; // contains all pieces of path for all modes
+      self.prevPosition = []; // contains all pieces of path for all modes
       model.planetModes.forEach( function( el, i ) {
         self.time[i] = 0;
         self.path[i] = {};
         self.prevPosition[i] = {};
         model.spaceObjects.forEach( function( el ) {
           self.path[i][el] = [];
-          self.prevPosition[i][el] = [model[el].position];
+          self.prevPosition[i][el] = model[el].position.copy();
         } );
       } );
     },
+    // remove all path for given mode
     clearOne: function( model, mode ) {
       var self = this;
       model.spaceObjects.forEach( function( el ) {
@@ -120,9 +135,10 @@ define( function( require ) {
           self.removeChild( obj.view );
         } );
         self.path[mode][el] = [];
-        self.prevPosition[mode][el] = [model[el].position];
+        self.prevPosition[mode][el] = model[el].position.copy();
       } );
     },
+    // hide all path for given mode
     hide: function( model, mode ) {
       var self = this;
       model.spaceObjects.forEach( function( el ) {
@@ -131,19 +147,17 @@ define( function( require ) {
         }
       } );
     },
+    // show all path for given mode
     show: function( model, mode ) {
       var self = this;
       model.spaceObjects.forEach( function( el ) {
+        // show all path for given mode
         for ( var i = 0; i < self.path[mode][el].length; i++ ) {
           self.path[mode][el][i].view.setVisible( true );
         }
 
-        if ( self.prevPosition[mode][el].length > 1 ) {
-          self.prevPosition[mode][el] = [self.prevPosition[mode][el].pop()];
-        }
-        else {
-          self.prevPosition[mode][el] = [model[el].position];
-        }
+        // set new previous position
+        self.prevPosition[mode][el] = model[el].position.copy();
       } );
     }
   } );
