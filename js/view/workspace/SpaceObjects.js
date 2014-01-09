2554875388c25c6ba5ca8940730b10005e76f27b
Sam Reid <reids@colorado.edu>
2014-01-08 23:57:21 -0800

    Fixed assertion error from removing a node that wasn't a child

--- js/view/workspace/SpaceObjects.js
+++ js/view/workspace/SpaceObjects.js
@@ -190,7 +190,10 @@ define( function( require ) {
       // set scale center
       model.scaleCenter = new Vector2( model.planetModes[num].options.centerX, model.planetModes[num].options.centerY );
 
-      this.removeChild( this.view );
+      //Remove the view, it will be discarded and garbage collected
+      if ( this.view && this.view.parentNode === this ) {
+        this.removeChild( this.view );
+      }
       model.scale = 1;
       model.day = 0;
       model.dayOffset = 0;
