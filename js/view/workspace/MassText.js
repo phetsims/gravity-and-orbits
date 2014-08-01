44c2065237d4e9875dc9c84025dadbc0a847093c
MLearner05 <andrey.zelenkov@mlearner.com>
2014-08-01 13:01:04 +0400

    #111 fixed bug with showing mass label after destroying planet

--- js/view/workspace/MassText.js
+++ js/view/workspace/MassText.js
@@ -31,7 +31,7 @@ define( function( require ) {
       var setMassText = function() {
         var text, defaultValue, precision, value;
 
-        if ( model.planetModes[model.planetMode][spaceObject] && !body.exploded ) {
+        if ( model.planetModes[model.planetMode][spaceObject] && body.massText.isVisible ) {
           text = model.planetModes[model.planetMode][spaceObject].massLabel.text;
           defaultValue = model.planetModes[model.planetMode][spaceObject].massLabel.defaultValue;
           precision = model.planetModes[model.planetMode][spaceObject].massLabel.precision || 0;
@@ -40,59 +40,46 @@ define( function( require ) {
           // if value ==== '1.00' replace 1.00 -> 1 and masses -> mass
           body.massText.setText( (value === '1.00' ? value.replace( '1.00', '1' ) + ' ' + text.substr( 0, text.length - 2 ) : value + ' ' + text) );
         }
-        else {
-          body.massText.setText( '' );
-        }
       };
 
       // update mass text position
       var setMassTextPosition = function() {
-        if ( !model.mass ) {return;} // check visibility
-        var height = ( isFinite( body.view.getHeight() ) ? body.view.getHeight() : 0),
-          positions = body.position;
+        var height, positions;
 
-        body.massText.setTranslation( positions.x * model.scale - body.massText.getWidth() / 2, (positions.y + height / 2) * model.scale + 15 );
+        if ( body.massText.isVisible ) {
+          height = ( isFinite( body.view.getHeight() ) ? body.view.getHeight() : 0);
+          positions = body.position;
+          body.massText.setTranslation( positions.x * model.scale - body.massText.getWidth() / 2, (positions.y + height / 2) * model.scale + 15 );
+        }
+      };
+
+      // update mass text visibility
+      var setMassTextVisibility = function() {
+        body.massText.setVisible( model.mass && model.planetModes[model.planetMode][spaceObject] && !body.exploded );
+      };
+
+      var updateMassText = function() {
+        setMassTextVisibility();
+        setMassText();
+        setMassTextPosition();
       };
 
       // add observers
-      body.explodedProperty.link( function( exploded ) {
-        if ( model.mass ) { // check visibility
-          body.massText.setVisible( !exploded );
-        }
-      } );
+      body.explodedProperty.link( setMassTextVisibility );
 
-      model.planetModeProperty.link( function() {
-        body.massText.setVisible( model.mass );
-        setMassText();
-        setMassTextPosition();
-      } );
+      model.massProperty.lazyLink( updateMassText );
+      model.planetModeProperty.link( updateMassText );
 
       body.massCoeffProperty.link( setMassText );
 
-      body.positionProperty.link( setMassTextPosition );
-
-      model.refreshModeProperty.link( function( trigger ) {
-        if ( trigger ) {
-          setMassTextPosition();
-        }
-      } );
-
-      body.radiusCoeffProperty.link( setMassTextPosition );
-
+      body.positionProperty.lazyLink( setMassTextPosition );
+      body.radiusCoeffProperty.lazyLink( setMassTextPosition );
+      model.refreshModeProperty.onValue( true, setMassTextPosition );
       model.scaleProperty.link( setMassTextPosition );
-
-      model.massProperty.link( function( visibility ) {
-        body.massText.setVisible( visibility );
-        setMassTextPosition();
-      } );
 
       // tune for different view modes
       if ( model.viewMode !== model.viewModes[1] ) {
         model.mass = false;
-      }
-      else {
-        setMassText();
-        setMassTextPosition();
       }
     } );
   }
