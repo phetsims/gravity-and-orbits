8e9749b2aaf84d29c76c7c2f90a65ea888c91139
Sam Reid <reids@colorado.edu>
2014-10-01 13:39:17 -0600

    Ported BodyState.js, see #138

--- js/gravity-and-orbits/model/BodyState.js
+++ js/gravity-and-orbits/model/BodyState.js
@@ -1,40 +1,42 @@
-//// Copyright 2002-2012, University of Colorado
-//
-//package edu.colorado.phet.gravityandorbits.model;
-//
-//import java.io.Serializable;
-//
-//import edu.colorado.phet.common.phetcommon.math.vector.Vector2D;
-//
-///**
-// * Immutable state returned by the physics engine update algorithm, it is applied to the mutable Body.
-// */
-//public class BodyState implements Serializable {
-//    public final Vector2D position;
-//    public final Vector2D velocity;
-//    public final Vector2D acceleration;
-//    public final double mass;
-//    public final boolean exploded;
-//
-//    public BodyState( Vector2D position, Vector2D velocity, Vector2D acceleration, double mass, boolean exploded ) {
-//        this.position = position;
-//        this.velocity = velocity;
-//        this.acceleration = acceleration;
-//        this.mass = mass;
-//        this.exploded = exploded;
-//    }
-//
-//    public double distanceSquared( Vector2D position ) {
-//        return this.position.minus( position ).magnitudeSquared();
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "BodyState{" +
-//               "position=" + position +
-//               ", velocity=" + velocity +
-//               ", acceleration=" + acceleration +
-//               ", mass=" + mass +
-//               '}';
-//    }
-//}
+// Copyright 2002-2012, University of Colorado
+
+/**
+ * Immutable state returned by the physics engine update algorithm, it is applied to the mutable Body.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+define( function( require ) {
+  'use strict';
+
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Serializable = require( 'java.io.Serializable' );
+  var Vector2D = require( 'edu.colorado.phet.common.phetcommon.math.vector.Vector2D' );
+
+  /**
+   *
+   * @param {Vector2} position
+   * @param {Vector2} velocity
+   * @param {Vector2} acceleration
+   * @param {number} mass
+   * @param {boolean} exploded
+   * @constructor
+   */
+  function BodyState( position, velocity, acceleration, mass, exploded ) {
+    this.position = position;
+    this.velocity = velocity;
+    this.acceleration = acceleration;
+    this.mass = mass;
+    this.exploded = exploded;
+  }
+
+  return inherit( Object, BodyState, {
+
+    distanceSquared: function( position ) {
+      return this.position.minus( position ).magnitudeSquared();
+    },
+
+    toString: function() {
+      return "BodyState{" + "position=" + position + ", velocity=" + velocity + ", acceleration=" + acceleration + ", mass=" + mass + '}';
+    }
+  } );
+} );
