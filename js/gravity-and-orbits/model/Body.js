efeccf3fb0ff85b77445f8287036fb0b7adaf2db
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-10-01 12:45:22 -0600

    Start porting Body.js from the Java code, see #138

--- js/gravity-and-orbits/model/Body.js
+++ js/gravity-and-orbits/model/Body.js
@@ -1,206 +1,258 @@
-//// Copyright 2002-2012, University of Colorado
+//  Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ * Body is a single point mass in the Gravity and Orbits simulation, such as the Earth, Sun, Moon or Space Station.
+ * This class also keeps track of body-related data such as the path.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Aaron Davis (PhET Interactive Simulations)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+
+  /**
+   *
+   * @param {IUserComponent} userComponent
+   * @param {string} name
+   * @param {number} x
+   * @param {number} y
+   * @param {number} diameter
+   * @param {number} vx
+   * @param {number} vy
+   * @param {number} mass
+   * @param {Color} color
+   * @param {Color} highlight
+   * @param {Function2<Body, Double, BodyRenderer>} renderer
+   * @param {number} labelAngle
+   * @param {boolean} massSettable
+   * @param {number} maxPathLength
+   * @param {boolean} massReadoutBelow
+   * @param {number} tickValue
+   * @param {string} tickLabel
+   * @param {Property<boolean>} playButtonPressed
+   * @param {Property<boolean>} stepping
+   * @param {Property<boolean>} rewinding
+   * @param {boolean} fixed
+   * @constructor
+   */
+  function Body( userComponent, name, x, y, diameter, vx, vy, mass, color, highlight, renderer,// way to associate the graphical representation directly instead of later with conditional logic or map
+                 labelAngle, massSettable, maxPathLength, massReadoutBelow, tickValue, tickLabel, playButtonPressed, stepping, rewinding, fixed ) {
+
+    PropertySet.call( this, {
+      acceleration: new Vector2(),
+      force: new Vector2(),
+      diameter: diameter, // number
+      clockTicksSinceExplosion: 0,
+      bounds: new Rectangle( 0, 0, 0, 0 ) //if the object leaves these model bounds, then it can be "returned" using a return button on the canvas
+    } );
+
+    this.userComponent = userComponent;//sun is immobile in cartoon mode
+    this.massSettable = massSettable;
+    this.maxPathLength = maxPathLength; //Number of samples in the path before it starts erasing (fading out from the back)
+
+    //True if the mass readout should appear below the body (so that readouts don't overlap too much),
+    // in the model for convenience since the body type determines where the mass readout should appear
+    this.massReadoutBelow = massReadoutBelow;
+    this.tickValue = tickValue; //value that this body's mass should be identified with, for 'planet' this will be the earth's mass
+    this.tickLabel = tickLabel; //name associated with this body when it takes on the tickValue above, for 'planet' this will be "earth"
+    this.fixed = fixed; //true if the object doesn't move when the physics engine runs, (though still can be moved by the user's mouse)
+    assert && assert( renderer != null );
+    this.name = name;
+    this.color = color;
+    this.highlight = highlight;
+    this.renderer = renderer; //function that creates a PNode for this Body.
+
+    // This is in the model so we can associate the graphical representation directly instead of later with conditional logic or map
+    this.labelAngle = labelAngle;
+    this.positionProperty = new RewindableProperty( playButtonPressed, stepping, rewinding, new Vector2( x, y ) );
+    this.velocityProperty = new RewindableProperty( playButtonPressed, stepping, rewinding, new Vector2( vx, vy ) );
+    this.massProperty = new RewindableProperty( playButtonPressed, stepping, rewinding, mass );
+    this.collidedProperty = new RewindableProperty( playButtonPressed, stepping, rewinding, false );
+    this.density = mass / getVolume();
+
+    this.userControlled = false;//True if the user is currently controlling the position of the body with the mouse
+    this.pathListeners = [];// ArrayList<PathListener>();
+    this.path = [];//new ArrayList<Vector2D>();
+
+    //list of listeners that are notified when the user drags the object, so that we know when certain properties need to be updated
+    this.userModifiedPositionListeners = [];//new ArrayList<VoidFunction0>();
+
+    collidedProperty.onValue( true, function() {
+      clockTicksSinceExplosion.set( 0 );
+    } );
+
+    //If any of the rewind properties changes while the clock is paused, set a rewind point for all of them.
+
+    //Relates to this problem reported by NP:
+    //NP: odd behavior with rewind: Open sim and press play, let the planet move to directly left of the sun.
+    //  Pause, then move the planet closer to sun. Press play, planet will move CCW. Then pause and hit rewind.
+    //  Press play again, the planet will start to move in the opposite direction (CW).
+    //SR: reproduced this in 0.0.14, perhaps the velocity is not being reset?
+    var thisBody = this;
+    var rewindValueChangeListener = function() {
+      thisBody.positionProperty.storeRewindValueNoNotify();
+      thisBody.velocityProperty.storeRewindValueNoNotify();
+      thisBody.massProperty.storeRewindValueNoNotify();
+      thisBody.collidedProperty.storeRewindValueNoNotify();
+    };
+    this.positionProperty.addRewindValueChangeListener( rewindValueChangeListener );
+    this.velocityProperty.addRewindValueChangeListener( rewindValueChangeListener );
+    this.massProperty.addRewindValueChangeListener( rewindValueChangeListener );
+    this.collidedProperty.addRewindValueChangeListener( rewindValueChangeListener );
+  }
+
+  return inherit( PropertySet, Body, {
+
+    /**
+     * @return {Property<number>}
+     */
+    getClockTicksSinceExplosion: function() {
+      return this.clockTicksSinceExplosion;
+    },
+
+    /**
+     * @return {number}
+     */
+    getVolume: function() {
+      return 4.0 / 3.0 * Math.PI * Math.pow( this.getRadius(), 3 );
+    },
+    /**
+     * @return {number}
+     */
+    getRadius: function() {
+      return this.getDiameter() / 2;
+    },
+
+    /**
+     * @return {Color}
+     */
+    getColor: function() {
+      return this.color;
+    },
+
+    /**
+     * @return {Color}
+     */
+    getHightlight: function() {
+      return this.highlight;
+    },
+    /**
+     * @return {RewindableProperty<Vector2>}
+     */
+    getPositionProperty: function() {
+      return positionProperty;
+    },
+
+    /**
+     * @return {Vector2}
+     */
+    getPosition: function() {
+      return positionProperty.get();
+    },
+
+    /**
+     * @return {Property<Double> }
+     */
+    getDiameterProperty: function() {
+      return diameterProperty;
+    },
+
+    /**
+     * @return {number}
+     */
+    getDiameter: function() {
+      return diameterProperty.get();
+    },
+
+    //TODO:
+    //   Clients are required to call notifyUserModifiedPosition if this translation was done by the user.
+    //   That's not at all clear (not documented here), it's error prone and it introduces order dependency.
+    //   Recommend making notifyUserModifiedPosition private and adding another public variant of translate,
+    //   i.e. public void translate(Point2D delta,boolean userModified) {...}
+
+    /**
+     * @param {Vector2} delta
+     */
+    translate: function( dx, dy ) {
+      if ( dx instanceof Vector2 ) {
+        dx = dx.x;
+        dy = dx.y;
+      }
+      this.positionProperty.set( new Vector2( this.getX() + dx, this.getY() + dy ) );
+
+      //Only add to the path if the object hasn't collided
+      //NOTE: this check was not originally in the 2 param translate method
+      if ( !this.collidedProperty.get() && !this.userControlled ) {
+        this.addPathPoint();
+      }
+    },
+
+    /**
+     * @return {number}
+     */
+    getY: function() {
+      return this.positionProperty.get().getY();
+    },
+
+    /**
+     * @return {number}
+     */
+    getX: function() {
+      return this.positionProperty.get().getX();
+    },
+
+    /**
+     * @return {string}
+     */
+    getName: function() {
+      return this.name;
+    },
+
+    /**
+     * @param {number} value
+     */
+    setDiameter: function( value ) {
+      this.diameterProperty.set( value );
+    },
+
+    /**
+     * create an immutable representation of this body for use in the physics engine
+     *
+     * @return {BodyState}
+     */
+    toBodyState: function() {
+      return new BodyState( this.getPosition(), this.getVelocity(), this.getAcceleration(), this.getMass(), this.collidedProperty.get() );
+    },
+
+    /**
+     * @return {number}
+     */
+    getMass: function() {
+      return this.massProperty.get();
+    },
+
+    /**
+     * @return {Vector2}
+     */
+    getAcceleration: function() {
+      return this.accelerationProperty.get();
+    },
+
+    /**
+     * @return {Vector2}
+     */
+    getVelocity: function() {
+      return this.velocityProperty.get();
+    }
+
+  } );
+} );
+
 //
-//package edu.colorado.phet.gravityandorbits.model;
-//
-//import java.awt.Color;
-//import java.awt.Shape;
-//import java.awt.geom.Point2D;
-//import java.awt.geom.Rectangle2D;
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//
-//import edu.colorado.phet.common.phetcommon.math.vector.Vector2D;
-//import edu.colorado.phet.common.phetcommon.model.property.Property;
-//import edu.colorado.phet.common.phetcommon.simsharing.messages.IUserComponent;
-//import edu.colorado.phet.common.phetcommon.util.SimpleObserver;
-//import edu.colorado.phet.common.phetcommon.util.function.Function2;
-//import edu.colorado.phet.common.phetcommon.util.function.VoidFunction0;
-//import edu.colorado.phet.gravityandorbits.view.BodyRenderer;
-//import edu.colorado.phet.gravityandorbits.view.IBodyColors;
-//import edu.colorado.phet.gravityandorbits.view.MultiwayOr;
-//
-///**
-// * Body is a single point mass in the Gravity and Orbits simulation, such as the Earth, Sun, Moon or Space Station.
-// * This class also keeps track of body-related data such as the path.
-// *
-// * @author Sam Reid
-// */
-//public class Body implements IBodyColors {
-//    private final RewindableProperty<Vector2D> positionProperty;
-//    private final RewindableProperty<Vector2D> velocityProperty;
-//    private final Property<Vector2D> accelerationProperty;
-//    private final Property<Vector2D> forceProperty;
-//    private final RewindableProperty<Double> massProperty;
-//    private final Property<Double> diameterProperty;
-//    private final String name;
-//    private final Color color;
-//    private final Color highlight;
-//    private final double density;
-//    private boolean userControlled;//True if the user is currently controlling the position of the body with the mouse
-//
-//    private final ArrayList<PathListener> pathListeners = new ArrayList<PathListener>();
-//    private final ArrayList<Vector2D> path = new ArrayList<Vector2D>();
-//    private final int maxPathLength; //Number of samples in the path before it starts erasing (fading out from the back)
-//
-//    private final boolean massSettable;
-//    private final Function2<Body, Double, BodyRenderer> renderer;//function that creates a PNode for this Body.  This is in the model so we can associate the graphical representation directly instead of later with conditional logic or map
-//    private final double labelAngle;
-//    private final boolean massReadoutBelow;//True if the mass readout should appear below the body (so that readouts don't overlap too much), in the model for convenience since the body type determines where the mass readout should appear
-//    private final RewindableProperty<Boolean> collidedProperty;
-//    private final Property<Integer> clockTicksSinceExplosion = new Property<Integer>( 0 );
-//    private double tickValue;//value that this body's mass should be identified with, for 'planet' this will be the earth's mass
-//    private String tickLabel;//name associated with this body when it takes on the tickValue above, for 'planet' this will be "earth"
-//
-//    private ArrayList<VoidFunction0> userModifiedPositionListeners = new ArrayList<VoidFunction0>();//list of listeners that are notified when the user drags the object, so that we know when certain properties need to be updated
-//    private Property<Shape> bounds = new Property<Shape>( new Rectangle2D.Double( 0, 0, 0, 0 ) );//if the object leaves these model bounds, then it can be "returned" using a return button on the canvas
-//    public final boolean fixed;//true if the object doesn't move when the physics engine runs, (though still can be moved by the user's mouse)
-//    private IUserComponent userComponent;
-//
-//    public Body( IUserComponent userComponent, final String name, double x, double y, double diameter, double vx, double vy, double mass, Color color, Color highlight,
-//                 Function2<Body, Double, BodyRenderer> renderer,// way to associate the graphical representation directly instead of later with conditional logic or map
-//                 double labelAngle, boolean massSettable,
-//                 int maxPathLength,
-//                 boolean massReadoutBelow, double tickValue, String tickLabel, Property<Boolean> playButtonPressed, Property<Boolean> stepping, Property<Boolean> rewinding,
-//                 boolean fixed ) {
-//        this.userComponent = userComponent;//sun is immobile in cartoon mode
-//        this.massSettable = massSettable;
-//        this.maxPathLength = maxPathLength;
-//        this.massReadoutBelow = massReadoutBelow;
-//        this.tickValue = tickValue;
-//        this.tickLabel = tickLabel;
-//        this.fixed = fixed;
-//        assert renderer != null;
-//        this.name = name;
-//        this.color = color;
-//        this.highlight = highlight;
-//        this.renderer = renderer;
-//        this.labelAngle = labelAngle;
-//        positionProperty = new RewindableProperty<Vector2D>( playButtonPressed, stepping, rewinding, new Vector2D( x, y ) );
-//        velocityProperty = new RewindableProperty<Vector2D>( playButtonPressed, stepping, rewinding, new Vector2D( vx, vy ) );
-//        accelerationProperty = new Property<Vector2D>( new Vector2D( 0, 0 ) );
-//        forceProperty = new Property<Vector2D>( new Vector2D( 0, 0 ) );
-//        massProperty = new RewindableProperty<Double>( playButtonPressed, stepping, rewinding, mass );
-//        diameterProperty = new Property<Double>( diameter );
-//        collidedProperty = new RewindableProperty<Boolean>( playButtonPressed, stepping, rewinding, false );
-//        density = mass / getVolume();
-//
-//        //Synchronize the scaled position, which accounts for the scale
-//        collidedProperty.addObserver( new SimpleObserver() {
-//            public void update() {
-//                if ( collidedProperty.get() ) {
-//                    clockTicksSinceExplosion.set( 0 );
-//                }
-//            }
-//        } );
-//
-//        //If any of the rewind properties changes while the clock is paused, set a rewind point for all of them.
-//
-//        //Relates to this problem reported by NP:
-//        //NP: odd behavior with rewind: Open sim and press play, let the planet move to directly left of the sun.
-//        //  Pause, then move the planet closer to sun. Press play, planet will move CCW. Then pause and hit rewind.
-//        //  Press play again, the planet will start to move in the opposite direction (CW).
-//        //SR: reproduced this in 0.0.14, perhaps the velocity is not being reset?
-//
-//        final VoidFunction0 rewindValueChangeListener = new VoidFunction0() {
-//            public void apply() {
-//                positionProperty.storeRewindValueNoNotify();
-//                velocityProperty.storeRewindValueNoNotify();
-//                massProperty.storeRewindValueNoNotify();
-//                collidedProperty.storeRewindValueNoNotify();
-//            }
-//        };
-//        positionProperty.addRewindValueChangeListener( rewindValueChangeListener );
-//        velocityProperty.addRewindValueChangeListener( rewindValueChangeListener );
-//        massProperty.addRewindValueChangeListener( rewindValueChangeListener );
-//        collidedProperty.addRewindValueChangeListener( rewindValueChangeListener );
-//    }
-//
-//    public Property<Integer> getClockTicksSinceExplosion() {
-//        return clockTicksSinceExplosion;
-//    }
-//
-//    private double getVolume() {
-//        return 4.0 / 3.0 * Math.PI * Math.pow( getRadius(), 3 );
-//    }
-//
-//    public double getRadius() {
-//        return getDiameter() / 2;
-//    }
-//
-//    public Color getColor() {
-//        return color;
-//    }
-//
-//    public Color getHighlight() {
-//        return highlight;
-//    }
-//
-//    public RewindableProperty<Vector2D> getPositionProperty() {
-//        return positionProperty;
-//    }
-//
-//    public Vector2D getPosition() {
-//        return positionProperty.get();
-//    }
-//
-//    public Property<Double> getDiameterProperty() {
-//        return diameterProperty;
-//    }
-//
-//    public double getDiameter() {
-//        return diameterProperty.get();
-//    }
-//
-//    //TODO:
-//    //   Clients are required to call notifyUserModifiedPosition if this translation was done by the user.
-//    //   That's not at all clear (not documented here), it's error prone and it introduces order dependency.
-//    //   Recommend making notifyUserModifiedPosition private and adding another public variant of translate,
-//    //   i.e. public void translate(Point2D delta,boolean userModified) {...}
-//    public void translate( Point2D delta ) {
-//        translate( delta.getX(), delta.getY() );
-//
-//        //Only add to the path if the object hasn't collided
-//        if ( !collidedProperty.get() && !userControlled ) {
-//            addPathPoint();
-//        }
-//    }
-//
-//    public void translate( double dx, double dy ) {
-//        positionProperty.set( new Vector2D( getX() + dx, getY() + dy ) );
-//    }
-//
-//    public double getY() {
-//        return positionProperty.get().getY();
-//    }
-//
-//    public double getX() {
-//        return positionProperty.get().getX();
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setDiameter( double value ) {
-//        diameterProperty.set( value );
-//    }
-//
-//    //create an immutable representation of this body for use in the physics engine
-//    public BodyState toBodyState() {
-//        return new BodyState( getPosition(), getVelocity(), getAcceleration(), getMass(), collidedProperty.get() );
-//    }
-//
-//    public double getMass() {
-//        return massProperty.get();
-//    }
-//
-//    public Vector2D getAcceleration() {
-//        return accelerationProperty.get();
-//    }
-//
-//    public Vector2D getVelocity() {
-//        return velocityProperty.get();
-//    }
+
 //
 //    //Take the updated BodyState from the physics engine and update the state of this body based on it.
 //    public void updateBodyStateFromModel( BodyState bodyState ) {
