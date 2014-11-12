aee63ebc15d438bef84809abcff4c4f5e4cd5814
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-11-12 09:27:08 -0700

    port BodyConfiguration

--- js/gravity-and-orbits/module/BodyConfiguration.js
+++ js/gravity-and-orbits/module/BodyConfiguration.js
@@ -1,32 +1,42 @@
-//// Copyright 2002-2012, University of Colorado
-//package edu.colorado.phet.gravityandorbits.module;
-//
-//import edu.colorado.phet.common.phetcommon.math.vector.Vector2D;
-//
-///**
-// * Class that facilitates configuration of body instances for a GravityAndOrbitsMode; a data structure that describes a body's parameters.
-// *
-// * @author Sam Reid
-// */
-//public class BodyConfiguration {
-//    public double mass;
-//    public double radius;
-//    public double x;
-//    public double y;
-//    public double vx;
-//    public double vy;
-//    public boolean fixed = false;//True if the object doesn't move when the clock ticks
-//
-//    public BodyConfiguration( double mass, double radius, double x, double y, double vx, double vy ) {
-//        this.mass = mass;
-//        this.radius = radius;
-//        this.x = x;
-//        this.y = y;
-//        this.vx = vx;
-//        this.vy = vy;
-//    }
-//
-//    public Vector2D getMomentum() {
-//        return new Vector2D( vx * mass, vy * mass );
-//    }
-//}
+// Copyright 2002-2014, University of Colorado
+
+/**
+ * Class that facilitates configuration of body instances for a GravityAndOrbitsMode; a data structure that describes a body's parameters.
+ *
+ * @author Sam Reid
+ * @author Aaron Davis
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Vector2 = require( 'DOT/Vector2' );
+
+  /**
+   *
+   * @param{number} mass
+   * @param{number} radius
+   * @param{number} x
+   * @param{number} y
+   * @param{number} vx
+   * @param{number} vy
+   * @constructor
+   */
+  function BodyConfiguration( mass, radius, x, y, vx, vy ) {
+    //True if the object doesn't move when the clock ticks
+    this.fixed = false;
+    this.mass = mass;
+    this.radius = radius;
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+  }
+
+  return inherit( Object, BodyConfiguration, {
+    getMomentum: function() {
+      return new Vector2( this.vx * this.mass, this.vy * this.mass );
+    }
+  } );
+} );
