361f4b350a2f61b00349354ab19061e5006daa71
Sam Reid <reids@colorado.edu>
2015-02-03 20:52:49 -0700

    Added cache buster as an option & standardized formatting for config files, see https://github.com/phetsims/joist/issues/196

--- js/gravity-and-orbits-config.js
+++ js/gravity-and-orbits-config.js
@@ -34,5 +34,6 @@ require.config( {
     GRAVITY_AND_ORBITS: '.'
   },
 
-  urlArgs: new Date().getTime()  // cache buster to make browser refresh load all included scripts
+  // optional cache buster to make browser refresh load all included scripts, can be disabled with ?cacheBuster=false
+  urlArgs: phet.phetcommon.getCacheBusterArgs()
 } );
