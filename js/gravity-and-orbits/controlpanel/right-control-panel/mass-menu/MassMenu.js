f91ce36cb57b5fb47c94b94a3b3627312993a324
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2015-04-04 14:45:47 -0600

    make mass sliders change with mode

--- js/gravity-and-orbits/controlpanel/right-control-panel/mass-menu/MassMenu.js
+++ js/gravity-and-orbits/controlpanel/right-control-panel/mass-menu/MassMenu.js
@@ -1,14 +1,11 @@
-// Copyright 2002-2013, University of Colorado Boulder
+// Copyright 2002-2015, University of Colorado Boulder
 
 /**
- * Container for mass sliders.
- * There should be two sliders.
- * Sliders can selected through variable showModes.
+ * Container for mass sliders, there should be two of them per mode.
  *
  * @author Andrey Zelenkov (Mlearner)
+ * @author Aaron Davis
  */
-
-
 define( function( require ) {
   'use strict';
 
@@ -18,33 +15,29 @@ define( function( require ) {
   var VBox = require( 'SCENERY/nodes/VBox' );
   var BodyMassControl = require( 'GRAVITY_AND_ORBITS/gravity-and-orbits/controlpanel/BodyMassControl' );
 
-  // strings
-  var ourSunString = require( 'string!GRAVITY_AND_ORBITS/ourSun' );
-  var earthString = require( 'string!GRAVITY_AND_ORBITS/earth' );
-  var ourMoonString = require( 'string!GRAVITY_AND_ORBITS/ourMoon' );
-  var spaceStationString = require( 'string!GRAVITY_AND_ORBITS/spaceStation' );
-  var starString = require( 'string!GRAVITY_AND_ORBITS/star' );
-  var planetString = require( 'string!GRAVITY_AND_ORBITS/planet' );
-  var moonString = require( 'string!GRAVITY_AND_ORBITS/moon' );
-  var satelliteString = require( 'string!GRAVITY_AND_ORBITS/satellite' );
-
   /**
    * @param {GravityAndOrbitsModule} module
+   * @param {Object} [options]
    * @constructor
    */
   function MassMenu( module, options ) {
 
-    var children = [];
-    var bodies = module.getMode().getModel().getBodies();
-    for ( var i = 0; i < bodies.length; i++ ) {
-      var body = bodies[ i ];
-      if ( body.isMassSettable() ) {
-        children.push( new BodyMassControl( body, body.getMassProperty().getInitialValue() / 2, body.getMassProperty().getInitialValue() * 2, body.getTickValue(), body.getTickLabel(), module.whiteBackgroundProperty ) );
-      }
-    }
-
-    options = _.extend( { children: children, resize: false }, options );
+    options = _.extend( { resize: false }, options );
     VBox.call( this, options );
+
+    var thisNode = this;
+    module.modeProperty.link( function( mode ) {
+      thisNode.removeAllChildren();
+      var bodies = mode.getModel().getBodies();
+      for ( var i = 0; i < bodies.length; i++ ) {
+        var body = bodies[ i ];
+        if ( body.isMassSettable() ) {
+          thisNode.addChild( new BodyMassControl( body, body.getMassProperty().getInitialValue() / 2, body.getMassProperty().getInitialValue() * 2, body.getTickValue(), body.getTickLabel(), module.whiteBackgroundProperty ) );
+        }
+      }
+      thisNode.updateLayout();
+
+    } );
   }
 
   return inherit( VBox, MassMenu );
