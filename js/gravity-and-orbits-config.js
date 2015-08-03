a940905fe953cbd89e7099c7611405bd1dc45916
Sam Reid <reids@colorado.edu>
2015-08-02 23:58:15 -0600

    Moved all brands to master, see https://github.com/phetsims/brand/issues/11

--- js/gravity-and-orbits-config.js
+++ js/gravity-and-orbits-config.js
@@ -23,7 +23,7 @@ require.config( {
 
     // PhET libs, uppercase names to identify them in require.js imports
     AXON: '../../axon/js',
-    BRAND: '../../brand/js',
+    BRAND: '../../brand/' + phet.chipper.getBrandName() + '/js',
     DOT: '../../dot/js',
     JOIST: '../../joist/js',
     KITE: '../../kite/js',
