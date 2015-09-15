df1b3b1467168e6af96bce7b409354e52845d0bb
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2015-09-15 10:46:38 -0600

    simplify RightControlPanel code

--- js/gravity-and-orbits/view/right-control-panel/BodyMassControl.js
+++ js/gravity-and-orbits/view/right-control-panel/BodyMassControl.js
@@ -77,7 +77,7 @@ define( function( require ) {
 
       // setting the diameter property took place in Body.setMass() in the Java version, but doesn't work here since
       // the mass itself is set by the slider in this case.
-      var radius = Math.pow( 3 * mass / 4 / Math.PI / body.density, 1 / 3 ); //derived from: density = mass/volume, and volume = 4/3 pi r r r
+      var radius = Math.pow( 3 * mass / 4 / Math.PI / body.density, 1 / 3 ); // derived from: density = mass/volume, and volume = 4/3 pi r r r
       body.diameterProperty.set( radius * 2 );
 
       // snap to default value if close
