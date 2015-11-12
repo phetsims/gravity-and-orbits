d14f3b188d9b50ad1b3bfa9595deffb35b0b3687
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2015-11-12 12:57:15 -0700

    factor out and document magic numbers, see #186

--- js/cartoon/module/CartoonModeList.js
+++ js/cartoon/module/CartoonModeList.js
@@ -17,24 +17,33 @@ define( function( require ) {
   var ModeList = require( 'GRAVITY_AND_ORBITS/common/module/ModeList' );
   var ModeListParameterList = require( 'GRAVITY_AND_ORBITS/common/module/ModeListParameterList' );
 
+  // constants
+  var SUN_RADIUS_MULTIPLIER = 50; // sun radius multiplier for SunEarthMode and SunEarthMoonMode, tuned by hand
+  var EARTH_MOON_RADIUS_MULTIPLIER = 800; // earth and moon radius multiplier for SunEarthMode and SunEarthMoonMode, tuned by hand
+  var EARTH_MASS_SCALE_FACTOR = 10200; // tuned by hand so there are 12 cartoon lunar orbits in one cartoon earth orbit
+
+  /*
+   * force scale for SunEarthMode and SunEarthMoonMode.
+   * balances increased mass and so that forces are 1/2 grid cell in default conditions, hand tuned by checking
+   * that reducing the distance by a factor of 2 increases the force arrow by a factor of 4
+   */
+  var FORCE_SCALE = 0.573 / EARTH_MASS_SCALE_FACTOR;
+
+  /*
+   * Have to artificially scale up the time readout so that SunEarthMode and SunEarthMoonMode modes have a stable
+   * orbits with correct periods since masses are nonphysical
+   */
+  var SUN_EARTH_MODE_TIME_SCALE = 365.0 / 334.0;
+
   function SunEarthModeConfig() {
 
     ModeList.SunEarthModeConfig.call( this );
 
-    this.sun.radius *= 50;
-    this.earth.radius *= 800;
-
-    // Tuned by hand so there are 12 cartoon lunar orbits in one cartoon earth orbit
-    var earthMassScaleFactor = 10200;
-    this.earth.mass *= earthMassScaleFactor;
-
-    // to balance increased mass and so that forces are 1/2 grid cell in default conditions, hand tuned by checking
-    // that reducing the distance by a factor of 2 increases the force arrow by a factor of 4
-    this.forceScale *= 0.573 / earthMassScaleFactor;
-
-    // Have to artificially scale up the time readout so that Sun/Earth/Moon mode has a stable orbit with correct
-    // periods since masses are nonphysical
-    this.timeScale = 365.0 / 334.0;
+    this.sun.radius *= SUN_RADIUS_MULTIPLIER;
+    this.earth.radius *= EARTH_MOON_RADIUS_MULTIPLIER;
+    this.earth.mass *= EARTH_MASS_SCALE_FACTOR;
+    this.forceScale *= FORCE_SCALE;
+    this.timeScale = SUN_EARTH_MODE_TIME_SCALE;
 
     // Sun shouldn't move in cartoon modes
     this.sun.fixed = true;
@@ -46,22 +55,16 @@ define( function( require ) {
 
     ModeList.SunEarthMoonModeConfig.call( this );
 
-    this.sun.radius *= 50;
-    this.earth.radius *= 800;
-    this.moon.radius *= 800;
+    this.sun.radius *= SUN_RADIUS_MULTIPLIER;
+    this.earth.radius *= EARTH_MOON_RADIUS_MULTIPLIER;
+    this.moon.radius *= EARTH_MOON_RADIUS_MULTIPLIER;
 
-    // Tuned by hand so there are 12 cartoon lunar orbits in one cartoon earth orbit
-    var earthMassScaleFactor = 10200;
-    this.earth.mass *= earthMassScaleFactor;
+    this.earth.mass *= EARTH_MASS_SCALE_FACTOR;
     this.moon.vx *= 21;
     this.moon.y = this.earth.radius * 1.7;
 
-    // to balance increased mass and so that forces are 1/2 grid cell in default conditions
-    this.forceScale *= 0.573 / earthMassScaleFactor;
-
-    // Have to artificially scale up the time readout so that Sun/Earth/Moon mode has a stable orbit with correct
-    // periods since masses are nonphysical
-    this.timeScale = 365.0 / 334.0;
+    this.forceScale *= FORCE_SCALE;
+    this.timeScale = SUN_EARTH_MODE_TIME_SCALE;
 
     // Sun shouldn't move in cartoon modes
     this.sun.fixed = true;
@@ -73,8 +76,9 @@ define( function( require ) {
 
     ModeList.EarthMoonModeConfig.call( this );
 
-    this.earth.radius *= 15;
-    this.moon.radius *= 15;
+    var radiusMultiplier = 15; // tuned by hand
+    this.earth.radius *= radiusMultiplier;
+    this.moon.radius *= radiusMultiplier;
 
     // so that default gravity force takes up 1/2 cell in grid
     this.forceScale *= 0.77;
@@ -86,6 +90,7 @@ define( function( require ) {
 
     ModeList.EarthSpaceStationModeConfig.call( this );
 
+    // tuned by hand
     this.earth.radius *= 0.8;
     this.spaceStation.radius *= 8;
   }
