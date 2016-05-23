d2039e239cb8707a8864be0f522ca3d20aa682d8
samreid <reids@colorado.edu>
2016-05-23 12:26:33 -0600

    Added ifphetio plugin and PHET_IO path to config file, see https://github.com/phetsims/phet-io/issues/453

--- js/gravity-and-orbits-config.js
+++ js/gravity-and-orbits-config.js
@@ -16,6 +16,7 @@ require.config( {
     image: '../../chipper/js/requirejs-plugins/image',
     mipmap: '../../chipper/js/requirejs-plugins/mipmap',
     string: '../../chipper/js/requirejs-plugins/string',
+    ifphetio: '../../chipper/js/requirejs-plugins/ifphetio',
 
     // third-party libs
     text: '../../sherpa/lib/text-2.0.12',
@@ -27,6 +28,7 @@ require.config( {
     JOIST: '../../joist/js',
     KITE: '../../kite/js',
     PHET_CORE: '../../phet-core/js',
+    PHET_IO: '../../phet-io/js',
     PHETCOMMON: '../../phetcommon/js',
     REPOSITORY: '..',
     SCENERY: '../../scenery/js',
