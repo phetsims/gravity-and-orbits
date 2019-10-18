765708bbbcc53043f734cbb140eab9cc4d8826ca
zepumph <michael.kauzmann@colorado.edu>
2019-10-17 16:12:07 -0800

    more _.extend conversion fixes, https://github.com/phetsims/phet-core/issues/71

--- js/common/view/GlobalOptionsNode.js
+++ js/common/view/GlobalOptionsNode.js
@@ -12,7 +12,6 @@ define( require => {
   // modules
   const gravityAndOrbits = require( 'GRAVITY_AND_ORBITS/gravityAndOrbits' );
   const GravityAndOrbitsColorProfile = require( 'GRAVITY_AND_ORBITS/common/GravityAndOrbitsColorProfile' );
-  const merge = require( 'PHET_CORE/merge' );
   const OptionsDialog = require( 'JOIST/OptionsDialog' );
   const ProjectorModeCheckbox = require( 'JOIST/ProjectorModeCheckbox' );
   const VBox = require( 'SCENERY/nodes/VBox' );
@@ -24,11 +23,11 @@ define( require => {
       const projectorModeCheckbox = new ProjectorModeCheckbox( GravityAndOrbitsColorProfile );
 
       // VBox is used to make it easy to add additional options
-      super( merge( {
+      super( {
         children: [ projectorModeCheckbox ],
         spacing: OptionsDialog.DEFAULT_SPACING,
         align: 'left'
-      } ) );
+      } );
     }
   }
 
