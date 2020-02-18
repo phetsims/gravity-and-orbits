dfb6884e407224f48ec166ef05c7d9d1e4871ff9
chrisklus <christopher.klusendorf@colorado.edu>
2020-02-18 14:32:01 -0500

    Update phetioDocumentation, see https://github.com/phetsims/joist/issues/591

--- js/phet-io/gravity-and-orbits-phet-io-elements-baseline.js
+++ js/phet-io/gravity-and-orbits-phet-io-elements-baseline.js
@@ -2147,7 +2147,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<ScreenIO>"
     },
     "gravityAndOrbits.general.soundManager.enabledProperty": {
-      "phetioDocumentation": "If this sim contains sound, then this Property will toggle whether sound is enabled ordisabled.",
+      "phetioDocumentation": "If the sim contains sound, then this Property will toggle whether sound is enabled or disabled.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
