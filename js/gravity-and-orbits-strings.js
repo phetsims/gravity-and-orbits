d01fb705c89e42ee29852065538e833e8a714d5d
samreid <reids@colorado.edu>
2020-02-27 12:07:58 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/gravity-and-orbits-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import gravityAndOrbits from './gravityAndOrbits.js';
+
+const gravityAndOrbitsStrings = getStringModule( 'GRAVITY_AND_ORBITS' );
+
+gravityAndOrbits.register( 'gravityAndOrbitsStrings', gravityAndOrbitsStrings );
+
+export default gravityAndOrbitsStrings;
