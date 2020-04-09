1938f0a09b322675cba4f045e07b5a07708777d5
zepumph <michael.kauzmann@colorado.edu>
2020-04-09 12:15:32 -0800

    rename mouseMovedAction -> mouseMoveAction, https://github.com/phetsims/scenery/issues/1043

--- js/phet-io/gravity-and-orbits-phet-io-elements-baseline.js
+++ js/phet-io/gravity-and-orbits-phet-io-elements-baseline.js
@@ -14,7 +14,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "ActionIO<Vector2IO, EventIO>"
     },
-    "gravityAndOrbits.general.controller.input.mouseMovedAction": {
+    "gravityAndOrbits.general.controller.input.mouseMoveAction": {
       "phetioDocumentation": "Emits when the mouse is moved The arguments are:<br/><ol><li>point: Vector2IO</li><br/><li>event: EventIO</li></ol>",
       "phetioDynamicElement": false,
       "phetioEventType": "USER",
