b7c8f54bd8f51b20096a36844b699e389a11db93
zepumph <michael.kauzmann@colorado.edu>
2020-04-10 11:50:26 -0800

    don't inherit methods in IO Types, https://github.com/phetsims/phet-io/issues/1623

--- js/phet-io/gravity-and-orbits-types.js
+++ js/phet-io/gravity-and-orbits-types.js
@@ -289,18 +289,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "documentation": "A dialog panel",
       "events": [],
       "methodOrder": [],
-      "methods": {
-        "moveBackward": {
-          "documentation": "Move this node one index backward in each of its parents.  If the node is already at the back, this is a no-op.",
-          "parameterTypes": [],
-          "returnType": "VoidIO"
-        },
-        "moveForward": {
-          "documentation": "Move this node one index forward in each of its parents.  If the node is already at the front, this is a no-op.",
-          "parameterTypes": [],
-          "returnType": "VoidIO"
-        }
-      },
+      "methods": {},
       "supertype": "NodeIO",
       "typeName": "DialogIO"
     },
@@ -779,35 +768,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
         "changed"
       ],
       "methodOrder": [],
-      "methods": {
-        "getValue": {
-          "documentation": "Gets the current value.",
-          "parameterTypes": [],
-          "returnType": "NumberIO"
-        },
-        "lazyLink": {
-          "documentation": "Adds a listener which will be called when the value changes. This method is like \"link\", but without the current-value callback on registration. The listener takes two arguments, the new value and the previous value.",
-          "parameterTypes": [
-            "FunctionIO(NumberIO,NullableIO<NumberIO>)=>VoidIO"
-          ],
-          "returnType": "VoidIO"
-        },
-        "link": {
-          "documentation": "Adds a listener which will be called when the value changes. On registration, the listener is also called with the current value. The listener takes two arguments, the new value and the previous value.",
-          "parameterTypes": [
-            "FunctionIO(NumberIO,NullableIO<NumberIO>)=>VoidIO"
-          ],
-          "returnType": "VoidIO"
-        },
-        "setValue": {
-          "documentation": "Sets the value of the Property. If the value differs from the previous value, listeners are notified with the new value.",
-          "invocableForReadOnlyElements": false,
-          "parameterTypes": [
-            "NumberIO"
-          ],
-          "returnType": "VoidIO"
-        }
-      },
+      "methods": {},
       "parameterTypes": [
         "NumberIO"
       ],
@@ -1576,18 +1537,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "documentation": "The tandem IO type for the scenery RichText node",
       "events": [],
       "methodOrder": [],
-      "methods": {
-        "moveBackward": {
-          "documentation": "Move this node one index backward in each of its parents.  If the node is already at the back, this is a no-op.",
-          "parameterTypes": [],
-          "returnType": "VoidIO"
-        },
-        "moveForward": {
-          "documentation": "Move this node one index forward in each of its parents.  If the node is already at the front, this is a no-op.",
-          "parameterTypes": [],
-          "returnType": "VoidIO"
-        }
-      },
+      "methods": {},
       "supertype": "NodeIO",
       "typeName": "RichTextIO"
     },
@@ -1681,18 +1631,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
         "toggled"
       ],
       "methodOrder": [],
-      "methods": {
-        "moveBackward": {
-          "documentation": "Move this node one index backward in each of its parents.  If the node is already at the back, this is a no-op.",
-          "parameterTypes": [],
-          "returnType": "VoidIO"
-        },
-        "moveForward": {
-          "documentation": "Move this node one index forward in each of its parents.  If the node is already at the front, this is a no-op.",
-          "parameterTypes": [],
-          "returnType": "VoidIO"
-        }
-      },
+      "methods": {},
       "supertype": "NodeIO",
       "typeName": "ToggleButtonIO"
     },
