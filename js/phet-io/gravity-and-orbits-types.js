3881626c45d52452cb8a396238d1cba016214712
pixelzoom <cmalley@pixelzoom.com>
2020-04-10 13:45:45 -0600

    revise IO Type doc for primitive core types, https://github.com/phetsims/tandem/issues/165

--- js/phet-io/gravity-and-orbits-types.js
+++ js/phet-io/gravity-and-orbits-types.js
@@ -212,7 +212,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "typeName": "ArrayIO<StringIO>"
     },
     "BooleanIO": {
-      "documentation": "Wrapper for the built-in JS boolean type (true/false)",
+      "documentation": "IO Type for Javascript's boolean primitive type",
       "events": [],
       "methodOrder": [],
       "methods": {},
@@ -766,7 +766,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "typeName": "NullableIO<Vector2IO>"
     },
     "NumberIO": {
-      "documentation": "Wrapper for the built-in JS number type (floating point, but also represents integers)",
+      "documentation": "IO Type for Javascript's number primitive type",
       "events": [],
       "methodOrder": [],
       "methods": {},
@@ -1633,7 +1633,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "typeName": "SliderIO"
     },
     "StringIO": {
-      "documentation": "Wrapper for the built-in JS string type",
+      "documentation": "IO Type for Javascript's string primitive type",
       "events": [],
       "methodOrder": [],
       "methods": {},
