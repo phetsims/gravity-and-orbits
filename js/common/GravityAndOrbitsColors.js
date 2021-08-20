77cef25e4d3053eb7234d67954c1559b85995bed
samreid <reids@colorado.edu>
2021-08-20 16:15:21 -0600

    Add Namespace parameter to ProfileColorProperty constructor, see https://github.com/phetsims/scenery/issues/1259

--- js/common/GravityAndOrbitsColors.js
+++ js/common/GravityAndOrbitsColors.js
@@ -7,33 +7,33 @@ import gravityAndOrbits from '../gravityAndOrbits.js';
 const GravityAndOrbitsColors = {
 
   // Color mainly used for foreground things like text
-  foregroundProperty: new ProfileColorProperty( 'foreground', { default: 'white', projector: 'black' }, {
+  foregroundProperty: new ProfileColorProperty( gravityAndOrbits, 'foreground', { default: 'white', projector: 'black' }, {
     tandem: Tandem.COLORS.createTandem( 'foregroundColorProperty' )
   } ),
 
   // Color mainly used for background things like panels or text backgrounds
-  backgroundProperty: new ProfileColorProperty( 'background', {
+  backgroundProperty: new ProfileColorProperty( gravityAndOrbits, 'background', {
     default: 'black',
     projector: 'white'
   }, {
     tandem: Tandem.COLORS.createTandem( 'backgroundColorProperty' )
   } ),
-  bodyLabelIndicatorProperty: new ProfileColorProperty( 'body label indicator', { default: new Color( 255, 255, 0 ), projector: 'black' }, {
+  bodyLabelIndicatorProperty: new ProfileColorProperty( gravityAndOrbits, 'body label indicator', { default: new Color( 255, 255, 0 ), projector: 'black' }, {
     tandem: Tandem.COLORS.createTandem( 'labelColorProperty' )
   } ),
 
-  measuringTapeTextBackgroundColorProperty: new ProfileColorProperty( 'measuring tape text background', {
+  measuringTapeTextBackgroundColorProperty: new ProfileColorProperty( gravityAndOrbits, 'measuring tape text background', {
     default: 'rgba( 0, 0, 0, 0.65 )',
     projector: 'rgba( 255, 255, 255, 0.65 )'
   }, {
     tandem: Tandem.COLORS.createTandem( 'measuringTapeTextBackgroundColorProperty' )
   } ),
 
-  gridIconStrokeColorProperty: new ProfileColorProperty( 'grid icon stroke', { default: 'gray', projector: 'black' }, {
+  gridIconStrokeColorProperty: new ProfileColorProperty( gravityAndOrbits, 'grid icon stroke', { default: 'gray', projector: 'black' }, {
     tandem: Tandem.COLORS.createTandem( 'gridIconStrokeColorProperty' )
   } ),
 
-  controlPanelFillProperty: new ProfileColorProperty( 'control panel fill', {
+  controlPanelFillProperty: new ProfileColorProperty( gravityAndOrbits, 'control panel fill', {
     default: 'black',
     projector: new Color( 222, 234, 255 )
   } )
