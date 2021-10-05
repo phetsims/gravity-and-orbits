5b2816539ecf40dbfa2a44cf427d1cdcf67cd8a3
Sam Reid <reids@colorado.edu>
2021-10-05 12:58:02 -0600

    Add tests, see https://github.com/phetsims/chipper/issues/1088

---
+++ js/common/model/RewindablePropertyTests.ts
@@ -0,0 +1,15 @@
+// Copyright 2021, University of Colorado Boulder
+
+import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
+import RewindableProperty from './RewindableProperty.js';
+import gravityAndOrbits from '../../gravityAndOrbits.js';
+
+QUnit.module( 'RewindablePropertyTests' );
+
+QUnit.test( 'Basic test', assert => {
+  assert.ok( true );
+
+  const r = new RewindableProperty( new BooleanProperty( false ), 'hello' );
+  r.set( 'bye' );
+  assert.ok( r.differentProperty.value === true, 'should be different' );
+} );
---
+++ js/gravity-and-orbits-tests.js
@@ -0,0 +1,13 @@
+// Copyright 2017-2021, University of Colorado Boulder
+
+/**
+ * Unit tests. Please run once in phet brand and once in brand=phet-io to cover all functionality.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+
+import './common/model/RewindablePropertyTests.js';
+import qunitStart from '../../chipper/js/sim-tests/qunitStart.js';
+
+// Since our tests are loaded asynchronously, we must direct QUnit to begin the tests
+qunitStart();
