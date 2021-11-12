fde8a06087ea319ec7ad5a4b004d8f99f82dfce2
Sam Reid <reids@colorado.edu>
2021-11-11 22:37:13 -0700

    Regenerate string file with grunt modulify, see https://github.com/phetsims/chipper/issues/1139

--- js/gravityAndOrbitsStrings.d.ts
+++
@@ -1,5 +0,0 @@
-// Copyright 2021, University of Colorado Boulder
-
-type t = { [ key: string ]: string };
-let gravityAndOrbitsStrings: t;
-export default gravityAndOrbitsStrings;
---
+++ js/gravityAndOrbitsStrings.ts
@@ -0,0 +1,61 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import gravityAndOrbits from './gravityAndOrbits.js';
+
+type StringsType = {
+  'gravity-and-orbits': {
+    'title': string
+  },
+  'earth': string,
+  'earthMasses': string,
+  'ourSun': string,
+  'earthDay': string,
+  'planet': string,
+  'planetMass': string,
+  'velocity': string,
+  'earthDays': string,
+  'measuringTape': string,
+  'billionBillionSpaceStationMasses': string,
+  'spaceStationMass': string,
+  'gravityForce': string,
+  'model': string,
+  'moon': string,
+  'moonMass': string,
+  'thousandEarthMasses': string,
+  'on': string,
+  'kilometers': string,
+  'pattern': {
+    '0value': {
+      '1units': string
+    }
+  },
+  'clear': string,
+  'spaceStation': string,
+  'satellite': string,
+  'satelliteMass': string,
+  'earthMinutes': string,
+  'gravity': string,
+  'grid': string,
+  'mass': string,
+  'off': string,
+  'path': string,
+  'ourMoon': string,
+  'earthMinute': string,
+  'returnObjects': string,
+  'star': string,
+  'starMass': string,
+  'earthMass': string,
+  'toScale': string,
+  'v': string
+};
+
+const gravityAndOrbitsStrings = getStringModule( 'GRAVITY_AND_ORBITS' ) as StringsType;
+
+gravityAndOrbits.register( 'gravityAndOrbitsStrings', gravityAndOrbitsStrings );
+
+export default gravityAndOrbitsStrings;
