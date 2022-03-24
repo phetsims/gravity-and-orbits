32686fa9dd0718caa5eca1fee12ce519f55448a2
phet-dev <phetadmin@gmail.com>
2022-03-24 03:17:27 -0600

    update copyright dates from daily grunt work

--- js/common/model/BodyConfiguration.ts
+++ js/common/model/BodyConfiguration.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2021, University of Colorado Boulder
+// Copyright 2014-2022, University of Colorado Boulder
 
 /**
  * Class that facilitates configuration of body instances for a GravityAndOrbitsScene; a data structure that describes
--- js/common/view/ExplosionNode.ts
+++ js/common/view/ExplosionNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2021, University of Colorado Boulder
+// Copyright 2014-2022, University of Colorado Boulder
 
 /**
  * Shows an explosion for a smaller Body when it crashes into a larger Body.
