c126afef9b71f30209223e6d8579a45ce759eb55
Sam Reid <reids@colorado.edu>
2022-08-30 23:21:13 -0600

    Use VerticalCheckboxGroup, see https://github.com/phetsims/scenery-phet/issues/766

--- js/common/view/CheckboxPanel.ts
+++ js/common/view/CheckboxPanel.ts
@@ -9,15 +9,13 @@
 
 import Property from '../../../../axon/js/Property.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
-import { Shape } from '../../../../kite/js/imports.js';
 import merge from '../../../../phet-core/js/merge.js';
 import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
 import ArrowNode from '../../../../scenery-phet/js/ArrowNode.js';
 import MeasuringTapeNode from '../../../../scenery-phet/js/MeasuringTapeNode.js';
 import PhetColorScheme from '../../../../scenery-phet/js/PhetColorScheme.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
-import { colorProfileProperty, HBox, HBoxOptions, Image, SceneryConstants, Text, VBox, VBoxOptions } from '../../../../scenery/js/imports.js';
-import Checkbox from '../../../../sun/js/Checkbox.js';
+import { colorProfileProperty, HBox, HBoxOptions, Image, SceneryConstants, Text } from '../../../../scenery/js/imports.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import iconMass_png from '../../../images/iconMass_png.js';
 import pathIcon_png from '../../../images/pathIcon_png.js';
@@ -28,6 +26,7 @@ import GravityAndOrbitsColors from '../GravityAndOrbitsColors.js';
 import GravityAndOrbitsGridNode from './GravityAndOrbitsGridNode.js';
 import GravityAndOrbitsModel from '../model/GravityAndOrbitsModel.js';
 import optionize, { combineOptions, EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
+import VerticalCheckboxGroup, { VerticalCheckboxGroupItem, VerticalCheckboxGroupOptions } from '../../../../sun/js/VerticalCheckboxGroup.js';
 
 // constants
 const FONT = new PhetFont( 18 );
@@ -51,9 +50,9 @@ const HBOX_OPTIONS = {
 
 type SelfOptions = EmptySelfOptions;
 
-type CheckboxPanelOptions = SelfOptions & VBoxOptions;
+type CheckboxPanelOptions = SelfOptions & VerticalCheckboxGroupOptions;
 
-class CheckboxPanel extends VBox {
+class CheckboxPanel extends VerticalCheckboxGroup {
 
   /**
    * @param model
@@ -61,41 +60,51 @@ class CheckboxPanel extends VBox {
    */
   public constructor( model: GravityAndOrbitsModel, providedOptions?: CheckboxPanelOptions ) {
 
-    const children = [];
-    const options = combineOptions<CheckboxPanelOptions>( { tandem: Tandem.OPTIONAL }, providedOptions );
-
     const gravityForceTextNode = new Text( gravityAndOrbitsStrings.gravityForceStringProperty, TEXT_OPTIONS );
     const velocityTextNode = new Text( gravityAndOrbitsStrings.velocityStringProperty, TEXT_OPTIONS );
     const massTextNode = new Text( gravityAndOrbitsStrings.massStringProperty, TEXT_OPTIONS );
     const pathTextNode = new Text( gravityAndOrbitsStrings.pathStringProperty, TEXT_OPTIONS );
     const gridTextNode = new Text( gravityAndOrbitsStrings.gridStringProperty, TEXT_OPTIONS );
     const measuringTapeTextNode = new Text( gravityAndOrbitsStrings.measuringTapeStringProperty, TEXT_OPTIONS );
-    const optionsWithTandem = ( tandemName: string ) => merge( { tandem: options.tandem!.createTandem( tandemName ) }, CHECKBOX_OPTIONS );
 
-    // gravity force checkbox
-    children.push( new Checkbox( model.showGravityForceProperty, new HBox( merge( {
-      children: [
-        gravityForceTextNode,
-        new ArrowNode( 135, ARROW_Y_COORDINATE, 180, ARROW_Y_COORDINATE, { fill: '#4380C2' } )
-      ]
-    }, HBOX_OPTIONS ) ), optionsWithTandem( 'gravityForceCheckbox' ) ) );
+    const items: VerticalCheckboxGroupItem[] = [ {
+      property: model.showGravityForceProperty,
+      createNode: tandem => new HBox( merge( {
+        children: [
+          gravityForceTextNode,
+          new ArrowNode( 135, ARROW_Y_COORDINATE, 180, ARROW_Y_COORDINATE, { fill: '#4380C2' } )
+        ]
+      }, HBOX_OPTIONS ) ),
+      tandemName: 'gravityForceCheckbox',
+      options: CHECKBOX_OPTIONS
+    }, {
 
-    // velocity checkbox
-    children.push( new Checkbox( model.showVelocityProperty, new HBox( merge( {
-      children: [
-        velocityTextNode,
-        new ArrowNode( 95, ARROW_Y_COORDINATE, 140, ARROW_Y_COORDINATE, { fill: PhetColorScheme.VELOCITY } )
-      ]
-    }, HBOX_OPTIONS ) ), optionsWithTandem( 'velocityCheckbox' ) ) );
+      // velocity checkbox
+      property: model.showVelocityProperty,
+      createNode: tandem => new HBox( merge( {
+        children: [
+          velocityTextNode,
+          new ArrowNode( 95, ARROW_Y_COORDINATE, 140, ARROW_Y_COORDINATE, { fill: PhetColorScheme.VELOCITY } )
+        ]
+      }, HBOX_OPTIONS ) ),
+      tandemName: 'velocityCheckbox',
+      options: CHECKBOX_OPTIONS
+    }
+    ];
 
     // mass checkbox
     if ( model.showMassCheckbox ) {
-      children.push( new Checkbox( model.showMassProperty, new HBox( merge( {
-        children: [
-          massTextNode,
-          new Image( iconMass_png, { scale: 0.8 } )
-        ]
-      }, HBOX_OPTIONS ) ), optionsWithTandem( 'massCheckbox' ) ) );
+      items.push( {
+        property: model.showMassProperty,
+        tandemName: 'massCheckbox',
+        createNode: tandem => new HBox( merge( {
+          children: [
+            massTextNode,
+            new Image( iconMass_png, { scale: 0.8 } )
+          ]
+        }, HBOX_OPTIONS ) ),
+        options: CHECKBOX_OPTIONS
+      } );
     }
 
     const pathIconImageNode = new Image( pathIcon_png, { scale: 0.25 } );
@@ -105,53 +114,58 @@ class CheckboxPanel extends VBox {
     } );
 
     // path checkbox
-    children.push( new Checkbox( model.showPathProperty, new HBox( merge( {
-      children: [
-        pathTextNode,
-        pathIconImageNode
-      ]
-    }, HBOX_OPTIONS ) ), optionsWithTandem( 'pathCheckbox' ) ) );
+    items.push( {
+      property: model.showPathProperty,
+      tandemName: 'pathCheckbox',
+      createNode: tandem => new HBox( merge( {
+        children: [
+          pathTextNode,
+          pathIconImageNode
+        ]
+      }, HBOX_OPTIONS ) ),
+      options: CHECKBOX_OPTIONS
+    } );
 
     // grid checkbox
-    children.push( new Checkbox( model.showGridProperty, new HBox( merge( {
-      children: [
-        gridTextNode,
-        new GravityAndOrbitsGridNode( new Property( ModelViewTransform2.createIdentity() ), 10, new Vector2( 0, 0 ), 1, {
-          stroke: GravityAndOrbitsColors.gridIconStrokeColorProperty,
-          lineWidth: 1.5
-        } )
-      ]
-    }, HBOX_OPTIONS ) ), optionsWithTandem( 'gridCheckbox' ) ) );
+    items.push( {
+      property: model.showGridProperty,
+      tandemName: 'gridCheckbox',
+      createNode: tandem => new HBox( merge( {
+        children: [
+          gridTextNode,
+          new GravityAndOrbitsGridNode( new Property( ModelViewTransform2.createIdentity() ), 10, new Vector2( 0, 0 ), 1, {
+            stroke: GravityAndOrbitsColors.gridIconStrokeColorProperty,
+            lineWidth: 1.5
+          } )
+        ]
+      }, HBOX_OPTIONS ) ),
+      options: CHECKBOX_OPTIONS
+    } );
 
     // measuring tape checkbox
     if ( model.showMeasuringTape ) {
       const measuringTapeIcon = MeasuringTapeNode.createIcon( { scale: 0.4 } );
-      children.push( new Checkbox( model.showMeasuringTapeProperty, new HBox( combineOptions<HBoxOptions>( {
-        align: 'top',
-        children: [
-          measuringTapeTextNode,
-          measuringTapeIcon
-        ]
-      }, HBOX_OPTIONS ) ), optionsWithTandem( 'measuringTapeCheckbox' ) ) );
+      items.push( {
+        property: model.showMeasuringTapeProperty,
+        tandemName: 'measuringTapeCheckbox',
+        createNode: tandem => new HBox( combineOptions<HBoxOptions>( {
+          align: 'top',
+          children: [
+            measuringTapeTextNode,
+            measuringTapeIcon
+          ]
+        }, HBOX_OPTIONS ) ),
+        options: CHECKBOX_OPTIONS
+      } );
     }
 
-    // increase the touch area of the checkboxes
-    const touchAreaHeight = 32;
-    for ( let i = 0; i < children.length; i++ ) {
-      const checkboxNode = children[ i ];
-      const bounds = checkboxNode.parentToLocalBounds( checkboxNode.bounds );
-      // @ts-ignore
-      checkboxNode.touchArea = Shape.rectangle( -5, bounds.centerY - touchAreaHeight / 2, bounds.width + 10, touchAreaHeight );
-    }
-
-    super( optionize<CheckboxPanelOptions, SelfOptions, VBoxOptions>()( {
-      excludeInvisibleChildrenFromBounds: true,
-      children: children,
+    const options = optionize<CheckboxPanelOptions, SelfOptions, VerticalCheckboxGroupOptions>()( {
       spacing: SPACING,
       align: 'left',
       bottom: -12,
       tandem: Tandem.REQUIRED
-    }, providedOptions ) );
+    }, providedOptions );
+    super( items, options );
   }
 }
 
