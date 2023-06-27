e6c8993a31b309a8501122389927c051d3d6613c
phet-dev <phetadmin@gmail.com>
2023-06-27 03:33:10 -0600

    update copyright dates from daily grunt work

--- js/common/GravityAndOrbitsScene.ts
+++ js/common/GravityAndOrbitsScene.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2022, University of Colorado Boulder
+// Copyright 2014-2023, University of Colorado Boulder
 
 /**
  * A GravityAndOrbitsScene behaves like a screen, it has its own model, control panel, canvas, and remembers its state
