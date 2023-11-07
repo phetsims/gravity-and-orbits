73b54428e091f130f558b14861c310743304b7ac
phet-dev <phetadmin@gmail.com>
2023-11-07 03:15:11 -0700

    update copyright dates from daily grunt work

--- js/common/model/RewindablePropertyTests.ts
+++ js/common/model/RewindablePropertyTests.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, University of Colorado Boulder
+// Copyright 2021-2023, University of Colorado Boulder
 
 import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
 import RewindableProperty from './RewindableProperty.js';
