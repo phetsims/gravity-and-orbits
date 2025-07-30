661143136c4de2e80afd64abcde8e50cd32dd78d
phet-dev <phetadmin@gmail.com>
2025-07-30 03:20:33 -0600

    update copyright dates from daily grunt work

--- js/gravity-and-orbits-tests.ts
+++ js/gravity-and-orbits-tests.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2024, University of Colorado Boulder
+// Copyright 2021-2025, University of Colorado Boulder
 
 /**
  * Unit tests. Please run once in phet brand and once in brand=phet-io to cover all functionality.
