b9fdbefa973b5876202d5201b76fc26d9c020f1c
phet-dev <phetadmin@gmail.com>
2025-08-28 04:07:14 -0600

    update copyright dates from daily grunt work

--- js/common/view/EarthMassReadoutNode.ts
+++ js/common/view/EarthMassReadoutNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2022, University of Colorado Boulder
+// Copyright 2014-2025, University of Colorado Boulder
 
 /**
  * Provides a textual readout of a Body's mass in "earth masses"
--- js/common/view/SpaceStationMassReadoutNode.ts
+++ js/common/view/SpaceStationMassReadoutNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2024, University of Colorado Boulder
+// Copyright 2014-2025, University of Colorado Boulder
 
 /**
  * Shows the mass of a Body in terms of space station masses.
